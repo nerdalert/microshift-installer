39f619b5dcffd626dfeafe13a61070bc2b12a9d5
Brent Salisbury <bsalisbu@redhat.com>
2022-02-01 01:35:36 -0500

    Added podman install to resolve microshift service failure
    - install podman as a pre-req
    - added some sleep statements to debug a low resource node install
    - adjust /etc/hosts to add the node host entry prior to installation

--- microshift-install.sh
+++ microshift-install.sh
@@ -1,21 +1,38 @@
 # Install script for https://github.com/redhat-et/microshift
-# Can be installed using: curl -sfL https://raw.githubusercontent.com/nerdalert/microshift-installer/main/microshift-install.sh |  sh -s -
+# Can be installed using: 
+# curl -sfL https://raw.githubusercontent.com/nerdalert/microshift-installer/main/microshift-install.sh |  sh -s -
 #!/bin/sh
 set -e
 set -o noglob
+
+# If the node does not have a FQDN
+export IP=$(hostname -I | awk '{print $1}')
+echo -e "- Adding the following host entry to /etc/hosts if it doesn't exist: $(hostname) $IP"
+if ! grep --quiet "$IP $hostname" /etc/hosts; then
+  echo $IP $(hostname) | sudo tee -a /etc/hosts
+fi
+echo -e "\xE2\x9C\x94 Done\n"
+
+echo '- Installing pre-requisites...'
+sudo dnf install -y podman
+echo -e "\xE2\x9C\x94 Done"
+sleep 15
 
 echo '- Installing cri-o container runtime...'
 sudo dnf module enable -y cri-o:1.21
 sudo dnf install -y cri-o cri-tools
 sudo systemctl enable crio --now
 echo -e "\xE2\x9C\x94 Done"
+sleep 30
 
 echo '- Installing Microshift packages...'
 sudo dnf copr enable -y @redhat-et/microshift
 sudo dnf install -y microshift firewalld
+sleep 30
 sudo systemctl enable microshift --now
 echo -e "\xE2\x9C\x94 Done"
 
+# wait up to 30 seconds for the kubeconfig file to become available
 count=1
 kubeconf=/var/lib/microshift/resources/kubeadmin/kubeconfig
 until sudo test -f $kubeconf;do
@@ -30,19 +47,11 @@ done
 echo "- Storing kubeconfig in ~/.kube/config"
 mkdir ~/.kube  &> /dev/null
 sudo cat /var/lib/microshift/resources/kubeadmin/kubeconfig > ~/.kube/config
-export KUBECONFIG=~/.kube/config
 echo -e "\xE2\x9C\x94 Done"
 
-export IP=$(hostname -I | awk '{print $1}')
-echo -e "- Adding the following host entry to /etc/hosts if it doesn't exist: $(hostname) $IP"
-if ! grep --quiet "$IP $hostname" /etc/hosts; then
-  echo $IP $(hostname) | sudo tee -a /etc/hosts
-fi
-echo -e "\xE2\x9C\x94 Done\n"
-
 echo "- Installation complete, view the pods with the following:"
-echo -e "kubectl get pods --all-namespaces -o wide\n"
+echo "cmd -> export KUBECONFIG=~/.kube/config"
+echo -e "cmd -> kubectl get pods --all-namespaces -o wide\n"
 echo "- Once all of the microshift pods are up and running, test the deployment with:"
-echo -e "kubectl apply -f https://k8s.io/examples/application/shell-demo.yaml  \n"
+echo -e "cmd -> kubectl apply -f https://k8s.io/examples/application/shell-demo.yaml  \n"
 echo "See documentation at https://microshift.io/docs/home/"
-
